f6e8030e5c5e608daa2444dc9801e4dfac7a58b3
Irina <irina.potrebko7@gmail.com>
2020-04-06 09:16:56 +0300

    create static UI

---
+++ src/Modal/Modal.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+class Modal extends React.Component {
+
+
+
+    render() {
+        return (
+            <div>
+                <button>show simple Modal</button>
+                <button>show question Modal</button>
+                <button>show input Modal</button>
+                <button>show message Modal</button>
+                <button>add success</button>
+                <button>add error</button>
+            </div>
+        );
+    }
+
+}
+
+
+
+export default Modal;
