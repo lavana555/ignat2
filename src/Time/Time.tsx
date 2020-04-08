0acdaef4a18b643f1e391764b5a97f9f3bfa444b
Roman Lazuka <rn.lazuka@gmail.com>
2020-04-08 13:48:48 +0300

    deleted broken code

--- src/Time/Time.tsx
+++ src/Time/Time.tsx
@@ -10,18 +10,11 @@ const Time: FC<PropsType> = (props) => {
     let [input1Value, setInput1Value] = useState(moment());
     let [input2Value, setInput2Value] = useState(moment());
     let newTime = moment.utc(input1Value.diff(input2Value));
-    let timeDiff = moment.utc(moment(input1Value).diff(input2Value)).format("HH:mm");
-
-    console.log(input1Value)
     let now = moment().format('HH:mm:ss');
     let showInputValue = (e: ChangeEvent<HTMLInputElement>) => {
         console.log(e.currentTarget.value)
     };
-    let changeInput1 = (e: any) => {
-        let newDate = moment().format( e.currentTarget.value)
-        setInput1Value(moment(newDate));
-        console.log(input1Value)
-    }
+
     return (
         <div>
             <div>
@@ -30,18 +23,12 @@ const Time: FC<PropsType> = (props) => {
             </div>
             <input type={'time'} onChange={showInputValue}/>
             <div>
-                <input type="time" value={input1Value.format("HH:mm")} onChange={changeInput1}/>
-                <input type="time" value={input2Value.format("HH:mm")}/>
-                <input type="time" value={timeDiff}/>
-            </div>
-            <div>
                 <TimePicker defaultValue={input1Value} format={"HH:mm"} showSecond={false} onChange={setInput1Value}/>---
                 <TimePicker defaultValue={input2Value} format={"HH:mm"} showSecond={false} onChange={setInput2Value}/>===
                 <TimePicker value={newTime} format={"HH:mm"} showSecond={false} showMinute={false} showHour={false}/>
             </div>
         </div>
     );
-
 }
 
 export default Time
