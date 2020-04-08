d17cb256a0b3532fdc08d66e1dfb90b39c51c729
Roman Lazuka <rn.lazuka@gmail.com>
2020-04-08 23:56:42 +0300

    added div with gradient color

--- src/Color/Color.tsx
+++ src/Color/Color.tsx
@@ -4,9 +4,14 @@ import React, {FC, useState} from 'react';
 const Color: FC<any> = (props) => {
     const [color, setColor] = useState('#212bde');
     return (
-        <div style={{display:"flex",flexDirection:"column",alignItems:"center"}}>
-            <input type={"color"} value={color} onChange={e=>setColor(e.currentTarget.value)}/>
-            <div style={{height:"100px", width:"100px", backgroundColor:color}}></div>
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+            <input type={"color"} value={color} onChange={e => setColor(e.currentTarget.value)}/>
+            <div style={{height: "100px", width: "100px", backgroundColor: color}}/>
+            <div style={{
+                height: '200px',
+                width: '500px',
+                background: 'linear-gradient(90deg,#000000, #EECFBA, #C5DDE8, #00ff00 )'
+            }}/>
         </div>
     );
 }
